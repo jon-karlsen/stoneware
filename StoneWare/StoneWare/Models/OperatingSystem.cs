5e43ec75dc4171e2997b8bb3fcee9735c0e8ac53
Jon Karlsen <karlsen.jonarild@gmail.com>
2020-02-05 14:47:53 +0100

    Initial
    Scaffold project, context and models from database, and add OperatingSystem seed data

---
+++ StoneWare/StoneWare/Models/OperatingSystem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoneWare.Models
+{
+    public class OperatingSystem
+    {
+        public OperatingSystem()
+        {
+            ProductVersionOs = new HashSet<ProductVersionOs>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<ProductVersionOs> ProductVersionOs { get; set; }
+    }
+}
---
+++ StoneWare/StoneWare/Models/Status.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoneWare.Models
+{
+    public class Status
+    {
+        public Status()
+        {
+            Issue = new HashSet<Issue>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Issue> Issue { get; set; }
+    }
+}
